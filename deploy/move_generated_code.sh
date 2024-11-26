1757391762d29c7ea6f79f3a23de9d3c8dbd689b
darkclown97 <shriarun97@gmail.com>
2024-11-27 01:42:33 +0530

    Added README.md

--- deploy/move_generated_code.sh
+++ deploy/move_generated_code.sh
@@ -1,6 +1,6 @@
 # Setup git credentials
-git config --global user.name "$GITHUB_USER_NAME"
-git config --global user.email "$GITHUB_USER_EMAIL"
+git config --global user.name "$REPO_USER_NAME"
+git config --global user.email "$REPO_USER_EMAIL"
 git clone https://github.com/darkclown97/api.git
 
 
@@ -17,4 +17,4 @@ cp -r ../generated/* .
 echo "Committing and pushing the changes..."
 git add .
 git commit -m "Update generated code"
-git push https://x-access-token:${GITHUB_TOKEN}@github.com/darkclown97/api.git master
+git push https://x-access-token:${REPO_TOKEN}@github.com/darkclown97/api.git master
